604de07387970e708e4f37600bcdf25e4c7b5f41
PatriciaMesa <patriciamesa.n1814@gmail.com>
2021-05-05 22:11:46 -0300

    Generate_Block

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-
+var bloqueRojo = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,7 +10,12 @@ extends TextureButton
 func _ready():
 	pass # Replace with function body.
 
-
+func generate_block():
+	var instanciaBloqueRojo = bloqueRojo.instance()
+	add_child(instanciaBloqueRojo)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_RedGenerator_button_down():
+	generate_block()
--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,6 +1,6 @@
 extends TextureButton
 
-
+var bloqueVioleta  = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -10,7 +10,12 @@ extends TextureButton
 func _ready():
 	pass # Replace with function body.
 
-
+func generate_block():
+	var instanciaBloqueVioleta = bloqueVioleta.instance()
+	add_child(instanciaBloqueVioleta)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_VioletGenerator_button_down():
+	generate_block()
